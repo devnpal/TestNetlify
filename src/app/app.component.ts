baec747b4863cd3ff82e78ad79ffb62aa3887dad
Devi Prasad Nepal <devnpl@hotmail.com>
2021-11-17 22:40:29 +0100

    button output customization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testnetlifyOne';
-  wife:any='';
-  
-  mywife(){
-	  this.wife ='ARCHANA';
+  wife: any = '';
+  count: number = 0;
+  message: string = '';
+  isJennifer: boolean = false;
+  lastName: string = '';
+  surName: string = '';
+
+  mywife() {
+    this.count += 1;
+
+    switch (this.count) {
+      case 1:
+        this.wife = 'ARCHANA';
+        this.isJennifer = false;
+        break;
+      case 2:
+        this.wife = 'ARCHANA ';
+        this.lastName = 'KHATIWADA';
+        this.isJennifer = false;
+        break;
+      case 3:
+        this.wife = 'ARCHANA ';
+        this.lastName = 'KHATIWADA';
+        this.surName = 'NEPAL';
+        this.isJennifer = false;
+        break;
+      case 4:
+        this.wife = '';
+        this.lastName = '';
+        this.surName = '';
+        this.message = 'What are you expecting ??'
+        this.isJennifer = false;
+        break;
+      case 5:
+        this.message = ''
+        this.wife = 'Jennifer Lawrence ??';
+        this.isJennifer = true;
+        break;
+      default:
+        this.wife = "NO...!!! MY Dear ARCHU.";
+        this.isJennifer = false;
+        break;
+    }
+
   }
 }
